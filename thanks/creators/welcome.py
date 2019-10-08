66a746fd4f48cba7ab3a6eb426238116db82a16b
Max Klein <isalix@gmail.com>
2019-10-08 14:52:36 -0700

    fix polling to account for different creation types.

--- thanks/creators/welcome.py
+++ thanks/creators/welcome.py
@@ -8,8 +8,10 @@ from civilservant.models.wikipedia import WikipediaUser
 from civilservant.util import ThingType, PlatformType
 
 import civilservant.logs
+
 civilservant.logs.initialize()
 import logging
+
 
 def welcome(db, batch_size, lang, intervention_name, intervention_type, config):
     """
@@ -33,23 +35,25 @@ def welcome(db, batch_size, lang, intervention_name, intervention_type, config):
     assert len(new_known_users) + len(new_unknown_users) == len(new_users)
 
     if not new_unknown_users:
-        #nobody new to report
+        # nobody new to report
         return []
     else:
         new_actions = []
         volunteer_signing_users = get_volunteer_signing_users(mwapi_session, lang)
         for new_unknown_user in new_unknown_users:
-
             wikipedia_user, experiment_thing, experiment_action = _create_new_user(db, lang, new_unknown_user,
                                                                                    intervention_name,
-                                                                                    intervention_type,
+                                                                                   intervention_type,
                                                                                    config, volunteer_signing_users)
-            db.add(wikipedia_user) # may already be done
-            db.add(experiment_thing)
-            db.add(experiment_action)
+            db.add(wikipedia_user)  # may already be done
+            if experiment_thing:
+                db.add(experiment_thing)
+            if experiment_action:
+                db.add(experiment_action)
+                new_actions.append(experiment_action)
             db.commit()
-            new_actions.append(experiment_action)
         return new_actions
+
 
 def _user_exists_in_db(db, lang, user_name):
     """
@@ -60,19 +64,50 @@ def _user_exists_in_db(db, lang, user_name):
     :return:
     """
     user_q = db.query(WikipediaUser).filter_by(lang=lang, user_name=user_name)
-    db_user = user_q.one_or_none()
-    #TODO check that ET also exists and raise assertion error if not
-    return True if db_user else False
+    db_users = user_q.all()
+    if len(db_users)>1:
+        logging.error('Found more than 1 already existing Wikipedia User. Something went wrong wrong.')
+    # TODO check that ET also exists and raise assertion error if not
+    return True if len(db_users)>0 else False
+
 
 def _create_new_user(db, lang, new_user, intervention_name, intervention_type, config, volunteer_signing_users):
+    """
+    create a wikipediaUser object, and if their creation_type is correct also create experimentThings and experimentAction
+    :param db:
+    :param lang:
+    :param new_user:
+    :param intervention_name:
+    :param intervention_type:
+    :param config:
+    :param volunteer_signing_users:
+    :return:
+    """
+
     # 0. create a wikipedia user
+    creation_type = new_user['action']
     wikipedia_user = WikipediaUser(lang=lang,
-                  user_name=new_user['user_name'],
-                  user_id=new_user['user_id'],
-                  user_registration=new_user['user_registration'])
-    # get the wikipediaUser id back
+                                   user_name=new_user['user_name'],
+                                   user_id=new_user['user_id'],
+                                   user_registration=new_user['user_registration'],
+                                   metadata_json={'creation_type': creation_type})
+
     db.add(wikipedia_user)
     db.commit()
+    # check if the creation_type is desired. this is for compatibility with pywikibot's welcome.py
+    # there are 4 creation types: create, create2, autocreate, byemail
+    # https://www.mediawiki.org/wiki/Manual:User_creation
+    logging.debug(f'Created WikipediaUser {wikipedia_user.user_name}, with creation_type {creation_type}')
+    if creation_type in ['create', 'autocreate']:
+        return _create_experiment_thing_actions(db, lang, wikipedia_user, intervention_name, intervention_type, config,
+                                                volunteer_signing_users)
+    else:
+        return wikipedia_user, None, None
+
+
+def _create_experiment_thing_actions(db, lang, wikipedia_user, intervention_name, intervention_type, config,
+                                     volunteer_signing_users):
+    # get the wikipediaUser id back
     wu_id = wikipedia_user.id
     # 1. get their randomization
     experiment_name = config['name']
@@ -86,29 +121,31 @@ def _create_new_user(db, lang, new_user, intervention_name, intervention_type, c
                                        object_platform=PlatformType.WIKIPEDIA,
                                        randomization_arm=randomization_arm,
                                        randomization_condition='welcome',
-                                       metadata_json={"randomization_block_id":randomization_block_id})
+                                       metadata_json={"randomization_block_id": randomization_block_id})
     # 3. create an action based on the randomization and user, and new mentor.
     signer = random.choice(volunteer_signing_users)
 
     experiment_action = ExperimentAction(experiment_id=experiment_id,
-                                     action_object_id=wu_id,
-                                     action_object_type=ThingType.WIKIPEDIA_USER,
-                                     action_subject_id=intervention_name,
-                                     action_subject_type=intervention_type,
-                                     action_platform=PlatformType.WIKIPEDIA,
-                                     action_key_id=randomization_arm,
-                                     action=intervention_type,
-                                     metadata_json={"randomization_arm":randomization_arm,
-                                                    "signer":signer,
-                                                    "lang":lang})
+                                         action_object_id=wu_id,
+                                         action_object_type=ThingType.WIKIPEDIA_USER,
+                                         action_subject_id=intervention_name,
+                                         action_subject_type=intervention_type,
+                                         action_platform=PlatformType.WIKIPEDIA,
+                                         action_key_id=randomization_arm,
+                                         action=intervention_type,
+                                         metadata_json={"randomization_arm": randomization_arm,
+                                                        "signer": signer,
+                                                        "lang": lang})
     return wikipedia_user, experiment_thing, experiment_action
 
 
 def _get_experiment_id(db, experiment_name):
     return db.query(Experiment.id).filter_by(name=experiment_name).one()
 
+
 def _get_num_experiment_things(db, experiment_id):
     return db.query(ExperimentThing).filter_by(experiment_id=experiment_id).count()
+
 
 def _get_next_randomization_arm(db, experiment_id, config):
     num_experiment_things = _get_num_experiment_things(db, experiment_id)
@@ -117,9 +154,9 @@ def _get_next_randomization_arm(db, experiment_id, config):
     randomizations = experiment.settings_json['randomizations']['data']
     randomziation_arm, randomization_block_id = randomizations[num_experiment_things]
     units_per_block = config['settings']['units_per_block']
-    expected_block_id  = math.floor(num_experiment_things / units_per_block)
+    expected_block_id = math.floor(num_experiment_things / units_per_block)
     logging.debug(f'''Correct block calculation num_experiment_things:{num_experiment_things} 
     units_per_block:{units_per_block} expected_block_id:{expected_block_id} actual_block_id:{randomization_block_id}''')
-    assert expected_block_id==randomization_block_id
+    assert expected_block_id == randomization_block_id
 
     return randomziation_arm, randomization_block_id,
