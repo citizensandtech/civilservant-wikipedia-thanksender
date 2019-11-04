6002de0c6c57db7b1c9389d022c0253730ab8d5d
Max Klein <isalix@gmail.com>
2019-11-04 15:49:01 -0800

    prove multiple query structure

--- thanks/reports/welcome_report.py
+++ thanks/reports/welcome_report.py
@@ -6,7 +6,11 @@ class WelcomeReport(EmailReport):
     def __init__(self):
         super().__init__()
 
-        now =  datetime.datetime.utcnow()
+        self.now = datetime.datetime.utcnow()
+        self.add_12_hourly()
+        self.add_deliverability()
+
+    def add_12_hourly(self):
         query_name = '12 hourly report'
         query_sql = '''select * from core_experiment_actions 
                             where experiment_id=%(experiment_id)s
@@ -14,15 +18,31 @@ class WelcomeReport(EmailReport):
                             and created_dt >= %(start_date)s
                             '''
         query_params = {'experiment_id': self.experiment_id,
-                        'start_date': now-datetime.timedelta(hours=12),
-                        'end_date': now
+                        'start_date': self.now - datetime.timedelta(hours=12),
+                        'end_date': self.now
                         }
         query_description = f'''Experiment Actions created in the last 12 hours.'''
-        super().add_query(query_name=query_name,
+        self.add_query(query_name=query_name,
                           query_sql=query_sql,
                           query_params=query_params,
                           query_description=query_description)
 
+    def add_deliverability(self):
+        query_name = 'deliverability report'
+        query_sql = '''select * from core_experiment_actions 
+                            where experiment_id=%(experiment_id)s
+                            and created_dt <= %(end_date)s
+                            and created_dt >= %(start_date)s
+                            '''
+        query_params = {'experiment_id': self.experiment_id,
+                        'start_date': self.now - datetime.timedelta(hours=12),
+                        'end_date': self.now
+                        }
+        query_description = f'''Experiment Actions created in the last 12 hours.'''
+        self.add_query(query_name=query_name,
+                          query_sql=query_sql,
+                          query_params=query_params,
+                          query_description=query_description)
 
 if __name__ == '__main__':
     wr = WelcomeReport()
