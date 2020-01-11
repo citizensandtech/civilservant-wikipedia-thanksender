462b1018ec6176e7b0359275e8530638ec0d71c7
notconfusing <max@notconfusing.com>
2020-01-11 01:15:54 +0000

    marke up for liasons

--- thanks/reports/welcome_report_liaisons.py
+++ thanks/reports/welcome_report_liaisons.py
@@ -7,8 +7,8 @@ class WelcomeReport(EmailReport):
         super().__init__()
 
         self.now = datetime.datetime.utcnow()
-        self.add_24_hourly()
         self.add_total_activity()
+        self.add_24_hourly()
 
     def add_24_hourly(self):
         query_name = '24 hourly report'
@@ -22,12 +22,12 @@ class WelcomeReport(EmailReport):
                             and created_dt <= %(end_date)s
                             and created_dt >= %(start_date)s
                             '''
-        twelve_hours_ago = self.now - datetime.timedelta(hours=24)
+        n_hours_ago = self.now - datetime.timedelta(hours=24)
         query_params = {'experiment_id': self.experiment_id,
-                        'start_date': twelve_hours_ago,
+                        'start_date': n_hours_ago,
                         'end_date': self.now
                         }
-        query_description = f'''Experiment actions created in the last between {twelve_hours_ago} and {self.now}.'''
+        query_description = f'''Experiment actions created in the last between {n_hours_ago} and {self.now}.'''
         subject_stat_fn = lambda df: f'{len(df)} new users welcomed in last 24 hours'
         self.add_query(query_name=query_name,
                        query_sql=query_sql,
@@ -43,7 +43,7 @@ class WelcomeReport(EmailReport):
                         from core_experiment_actions where experiment_id=%(experiment_id)s
                         group by metadata_json->'$.action_complete';'''
         query_params = {'experiment_id': self.experiment_id}
-        query_description = f'''Total activity ever in the welcome experiment.'''
+        query_description = f'''Total activity ever in the welcome experiment. Completed users mean that we were successfully able to post on their talk page. Uncompleted means they were blocked by the time we tried to edit their pages, or perhaps there was a network error and will retry to welcome them again soon.'''
         self.add_query(query_name=query_name,
                        query_sql=query_sql,
                        query_params=query_params,
